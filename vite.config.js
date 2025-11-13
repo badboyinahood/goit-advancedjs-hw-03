eb65e3624aa34260a1af8fcc5a9d67aa840dc42e
Konstantin <65172983+badboyinahood@users.noreply.github.com>
2025-11-13 07:28:38 +0200

    123

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/goit-advancedjs-hw-03/',
   root: 'src',
-  build: { outDir: '../dist', 
-    emptyOutDir: true },
+  base: '/goit-advancedjs-hw-03/',
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 
   define: {
     global: 'window',
