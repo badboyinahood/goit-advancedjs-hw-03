0f7f692271b3fb49e7f1d15d4517dcce5d6db866
Konstantin <65172983+badboyinahood@users.noreply.github.com>
2025-11-13 06:22:38 +0200

    123

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,38 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import fullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+  base: '/goit-advancedjs-hw-03/',
+
   root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
+  publicDir: 'src/public',
+
   define: {
     global: 'window',
   },
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: false,
+
+    rollupOptions: {
+      input: glob.sync('./src/index.html'), 
+
+      manualChunks(id) {
+        if (id.includes('node_modules')) return 'vendor';
+      },
+
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
+
+  plugins: [
+    injectHTML(),
+    fullReload(['./src/**/*.html']),
+  ],
 });
